09bd8e35a3dfd4a2c1c1ff0ad3f687df53131430
tanvirsingh551 <tanvir.singh05200704@gmail.com>
2026-07-04 17:34:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int findUnique(vector<int> &arr) {
+        int temp = arr[0];
+
+        for (int i = 1; i < arr.size(); i++) {
+            temp = temp ^ arr[i];
+        }
+
+        return temp;
+    }
+};
